b656c0b54af6c57b51ec95e7b2eb64edbdd648c4
Jianping Meng <jpmeng@gmail.com>
2025-09-08 09:54:04 +0100

    Correct the quotation error

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = ""pydata_sphinx_theme" #'alabaster'
+html_theme = "pydata_sphinx_theme" #'alabaster'
 
 # Enable theme switcher
 html_theme_options = {
